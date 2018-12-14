ff94bd3b7371d1bb4e092850a99a09ce9ddbbee2
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-12-14 07:31:19 +0100

    standard --fix (patch)

--- test/lib/inputs-test.js
+++ test/lib/inputs-test.js
@@ -7,7 +7,7 @@ tap.throws(
   function () {
     gpc()
   },
-  {message: 'Missing required param: pinLength'},
+  { message: 'Missing required param: pinLength' },
   'Throws if length number is not supplied'
 )
 
@@ -15,6 +15,6 @@ tap.throws(
   function () {
     gpc(8.9)
   },
-  {message: 'pinLength is not a whole number'},
+  { message: 'pinLength is not a whole number' },
   'Should require pincodeLength to be a whole number'
 )
